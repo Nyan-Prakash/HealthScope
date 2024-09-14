474835027b80aaca2acf609cac195497a934d91b
Shaunak <shaunakduude@gmail.com>
2024-09-14 00:31:10 -0400

    Added basic neural network

--- backend/neuralnet.py
+++ backend/neuralnet.py
@@ -2,6 +2,20 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 
-def neural_net():
-    x_train_data = np.random.random((1000, 10))
-    y_train_data = np.random.random(2, size=(1000, 1))
+x_train_data = np.random.random((1000, 10))
+y_train_data = np.random.randint(2, size=(1000, 1))
+
+model = Sequential()
+model.add(Dense(10, activation='relu', input_dim=10))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='adam', loss = 'binary_crossentropy', metrics=['accuracy'])
+
+model.fit(x_train_data, y_train_data, epochs=20, batch_size=10)
+
+x_test_data = np.random.random((100, 10))
+y_test_data = np.random.randint(2, size=(100, 1))
+
+loss, accuracy = model.evaluate(x_test_data, y_test_data)
+print('Test model loss:', loss)
+print('Test model accuracy:', accuracy)
